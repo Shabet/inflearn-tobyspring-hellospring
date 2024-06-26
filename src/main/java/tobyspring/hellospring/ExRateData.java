6698066f18c27fb571b558a3ee3ca04a14233ad7
Shabet <pakjunyoung@nate.com>
2024-06-26 23:10:59 +0900

    section01 내용 추가

--- src/main/java/tobyspring/hellospring/ExRateData.java
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -1,2 +1,10 @@
-package tobyspring.hellospring;public record ExRateData() {
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
 }
--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,2 +1,58 @@
-package tobyspring.hellospring;public class Payment {
+package tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreinCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreinCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreinCurrencyAmount = foreinCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeinCurrencyAmount() {
+        return foreinCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreinCurrencyAmount=" + foreinCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
