a45ed1c95ab811575527c97a15254190d4ed7918
Shabet <pakjunyoung@nate.com>
2024-06-29 14:41:31 +0900

    section03 내용 추가

--- src/main/java/tobyspring/hellospring/payment/PaymentService.java
+++ src/main/java/tobyspring/hellospring/payment/PaymentService.java
@@ -2,20 +2,28 @@ package tobyspring.hellospring.payment;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 public class PaymentService {
     private final ExRateProvider exRateProvider;
+    private final Clock clock;
 
-    public PaymentService(ExRateProvider exRateProvider) {
+    public PaymentService(ExRateProvider exRateProvider, Clock clock) {
         this.exRateProvider = exRateProvider;
+        this.clock = clock;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreinCurrencyAmount) throws IOException {
+/*
         BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreinCurrencyAmount.multiply(exRate);
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+        LocalDateTime validUntil = LocalDateTime.now(clock).plusMinutes(30);
 
         return new Payment(orderId, currency, foreinCurrencyAmount, exRate, convertedAmount, validUntil);
+*/
+
+        BigDecimal exRate = exRateProvider.getExRate(currency);
+        return Payment.createPrepared(orderId, currency, foreinCurrencyAmount, exRate, LocalDateTime.now(clock));
     }
 }
