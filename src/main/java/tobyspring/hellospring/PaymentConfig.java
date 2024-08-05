2a519da148d27ddf9b4254b3c14030d8e9ca69f9
Shabet <pakjunyoung@nate.com>
2024-08-05 11:05:06 +0900

    section04 스프링이 제공하는 템플릿

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,10 +2,13 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExtractor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
@@ -33,8 +36,15 @@ public class PaymentConfig {
     }
 
     @Bean
+    public RestTemplate restTeamplate() {
+//        return new RestTemplate();
+        return new RestTemplate(new JdkClientHttpRequestFactory());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider(apiTemplate());
+        return new RestTemplateExRateProvider(restTeamplate());
+//        return new WebApiExRateProvider(apiTemplate());
 //        return new SimpleExRateProvider();
     }
 
